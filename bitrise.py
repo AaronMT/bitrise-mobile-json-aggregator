ecef87de542e891b2cdcb79f37ded5d442102e1b
Aaron Train <aaron.train@gmail.com>
2021-02-22 15:43:01 -0500

    add additional args

--- bitrise.py
+++ bitrise.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import argparse
 import requests
+from datetime import datetime
 from enum import Enum
 
 _logger = logging.getLogger(__name__)
@@ -27,6 +28,36 @@ def parse_args(cmdln_args):
         choices=['ios', 'android']
     )
 
+    parser.add_argument(
+        "--branch",
+        help="Use selected branch",
+        required=False,
+        default=None
+    )
+
+    parser.add_argument(
+        "--workflow",
+        help="Use selected workflow",
+        required=False,
+        default=None
+    )
+
+    parser.add_argument(
+        "--before",
+        help="List builds run before a given date (Unix Timestamp)",
+        required=False,
+        default=None,
+        type=datetime.fromisoformat
+    )
+
+    parser.add_argument(
+        "--after",
+        help="List builds run after a given date (Unix Timestamp)",
+        required=False,
+        default=None,
+        type=datetime.fromisoformat
+    )
+
     return parser.parse_args(args=cmdln_args)
 
 
@@ -36,11 +67,6 @@ class Status(Enum):
     FAILURE = 2
     ABORTED_FAILURE = 3
     ABORTED_SUCCESS = 4
-
-
-class Workflows(Enum):
-    COMMON = "CommonBuild-UItest-XCUISmoketest"
-    NEW_XCODE = "NewXcodeVersions"
 
 
 class Bitrise:
@@ -93,11 +119,11 @@ class Bitrise:
             raise _logger.error('GET /apps/ {}'.format(resp.status_code))
         return resp.json()
 
-    def get_builds(self):
+    def get_builds(self, params=None):
         resp = \
             requests.get('https://api.bitrise.io/v0.1/apps/{0}'
                          '/builds'.format(self.APP_SLUG),
-                         headers=self.API_HEADER)
+                         headers=self.API_HEADER, params=params)
         if resp.status_code != 200:
             raise _logger.error('GET /apps/ {}'.format(resp.status_code))
         return resp.json()
@@ -179,8 +205,13 @@ def main():
 
     b.set_app(args.project, all_apps)
     workflows = b.get_workflows(b.APP_SLUG)
+    builds = b.get_builds({"branch": args.branch,
+                           "workflow": args.workflow,
+                           "after": int(datetime.timestamp(args.after))
+                           if args.after is not None else None,
+                           "before": int(datetime.timestamp(args.before))
+                           if args.before is not None else None})
 
-    builds = b.get_builds()
     failures = b.get_failure_count(builds)
     print("All recent builds Failure count: {0}".format(failures))
 
@@ -191,22 +222,12 @@ def main():
     print("All recent builds Not finished count: {0}".format(not_finished))
 
     aborted_failure = b.get_aborted_failure(builds)
-    print("Aborted failure count: {0}".format(aborted_failure))
+    print("All recent builds Aborted Failure count: {0}"
+          .format(aborted_failure))
 
     aborted_success = b.get_aborted_success(builds)
-    print("Aborted success count: {0}".format(aborted_success))
-
-    builds = b.get_builds_for_workflow(
-        builds, Workflows.NEW_XCODE.value)
-
-    failures = b.get_failure_count(builds)
-    print("Failure count for workflow: {0}".format(failures))
-
-    successes = b.get_success_count(builds)
-    print("Success count for workflow: {0}".format(successes))
-
-    not_finished = b.get_not_finished_count(builds)
-    print("No finished count: {0}".format(not_finished))
+    print("All recent builds Aborted Success count: {0}"
+          .format(aborted_success))
 
     print(*workflows['data'], sep=", ")
 
